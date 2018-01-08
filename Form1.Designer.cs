1462fe71d1bb0e72ebba02f226167f0c84d7b934
hauntps <haunt.hcm2015@gmail.com>
2018-01-08 12:27:49 +0700

    Compiler Simple C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace SimpleCompiler
+{
+    partial class compile_code
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtSourceCode = new System.Windows.Forms.TextBox();
+            this.cbxInfoDebug = new System.Windows.Forms.CheckBox();
+            this.txtClassName = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApplicationName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(846, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuTop";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as ..";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtSourceCode);
+            this.panel1.Controls.Add(this.cbxInfoDebug);
+            this.panel1.Controls.Add(this.txtClassName);
+            this.panel1.Controls.Add(this.btnRun);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtApplicationName);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(822, 480);
+            this.panel1.TabIndex = 2;
+            // 
+            // txtSourceCode
+            // 
+            this.txtSourceCode.Location = new System.Drawing.Point(3, 3);
+            this.txtSourceCode.Multiline = true;
+            this.txtSourceCode.Name = "txtSourceCode";
+            this.txtSourceCode.Size = new System.Drawing.Size(632, 474);
+            this.txtSourceCode.TabIndex = 9;
+            // 
+            // cbxInfoDebug
+            // 
+            this.cbxInfoDebug.AutoSize = true;
+            this.cbxInfoDebug.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxInfoDebug.Location = new System.Drawing.Point(655, 159);
+            this.cbxInfoDebug.Name = "cbxInfoDebug";
+            this.cbxInfoDebug.Size = new System.Drawing.Size(134, 17);
+            this.cbxInfoDebug.TabIndex = 8;
+            this.cbxInfoDebug.Text = "Debug Information ";
+            this.cbxInfoDebug.UseVisualStyleBackColor = true;
+            // 
+            // txtClassName
+            // 
+            this.txtClassName.Location = new System.Drawing.Point(655, 120);
+            this.txtClassName.Name = "txtClassName";
+            this.txtClassName.Size = new System.Drawing.Size(164, 20);
+            this.txtClassName.TabIndex = 7;
+            // 
+            // btnRun
+            // 
+            this.btnRun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(654, 201);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(105, 31);
+            this.btnRun.TabIndex = 6;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(654, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Class Name";
+            // 
+            // txtApplicationName
+            // 
+            this.txtApplicationName.Location = new System.Drawing.Point(654, 34);
+            this.txtApplicationName.Name = "txtApplicationName";
+            this.txtApplicationName.Size = new System.Drawing.Size(165, 20);
+            this.txtApplicationName.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(654, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Application Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // compile_code
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 519);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "compile_code";
+            this.Text = "Simple Compiler C# - Trung Nguyen";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtApplicationName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox txtClassName;
+        private System.Windows.Forms.CheckBox cbxInfoDebug;
+        private System.Windows.Forms.TextBox txtSourceCode;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace SimpleCompiler
+{
+    public partial class compile_code : Form
+    {
+        public compile_code()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            ////Microsoft.VisualBasic.VBCodeProvider ICodeCompiler compiler = codeProvider.CreateCompiler();
+            ICodeCompiler compiler = codeProvider.CreateCompiler();
+            CompilerParameters parameters = new CompilerParameters();
+
+            parameters.GenerateExecutable = true;
+            if (txtApplicationName.Text == "")
+            {
+                MessageBox.Show(this, "Application Name Can't Empty");
+                return;
+            }
+
+
+            parameters.OutputAssembly = txtApplicationName.Text.ToString();
+            if (txtClassName.Text == "")
+            {
+                MessageBox.Show(this, "Class Name Can't Empty");
+                return;
+            }
+            parameters.MainClass = txtClassName.Text;
+            parameters.IncludeDebugInformation = cbxInfoDebug.Checked;
+
+            foreach(Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                parameters.ReferencedAssemblies.Add(asm.Location);
+            }
+
+
+            String source = txtSourceCode.Text.ToString();
+            CompilerResults results = compiler.CompileAssemblyFromSource(parameters, source);
+
+            if (results.Errors.Count > 0)
+            {
+                string errors = "Compiler Some Issue Error: \n";
+                foreach (CompilerError err in results.Errors)
+                {
+                    errors += err.ToString() + "\n";
+                }
+                MessageBox.Show(this, errors, "Can't Execute. Please Try Again");
+            }
+            else
+            {
+                try
+                {
+                    if (!System.IO.File.Exists(txtApplicationName.Text.ToString()))
+                    {
+                        MessageBox.Show(String.Format("Don't Exists Application Name: {0}", txtApplicationName),
+                            "Can't Execute Program.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    ProcessStartInfo pInfo = new ProcessStartInfo(txtApplicationName.Text.ToString());
+                    Process.Start(pInfo);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(String.Format("Some Issue {0}", txtApplicationName) + ex.ToString(),
+                            "Can't Execute.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+
+        }
+    }
+}
